2b33f903317b6dcd86d26b5fa9b7145d600260de
Annopaolo <arnaldo.cesco@gmail.com>
2020-07-22 12:46:05 +0200

    shooting sound

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    Vector2 target;
+    AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<Rigidbody2D>().position;
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shoot");
+            sound.Play();
+        }
+    }
+}
