f6e782079382d1d10bf04bf97f4af4c24f8ce68a
DavideZan <68640133+DavideZan@users.noreply.github.com>
2020-07-22 12:17:22 +0200

    Add files via upload

---
+++ Assets/Script/GeneralMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralMovement : MonoBehaviour
+{
+    Rigidbody2D rigi;
+    public float speed = 10;
+    private void Awake ()
+   
+   
+    {
+        rigi = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigi.velocity = transform.right * speed * Time.deltaTime;
+    }
+}
