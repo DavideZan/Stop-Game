2b024a493ff3a41808293d6cc8386a2725c140ba
Davide <zanna_davide@hotmail.it>
2020-08-01 00:08:55 +0200

    MonsterShoot and BulletScript
    I've to learn how to make fun and enjoyable summaries, matter of time i guess

---
+++ Assets/Script/Common/BulletScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    ShootingSceneController controller;
+
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime); 
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyBullet();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            controller.KillPlayer();
+        }
+    }
+
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/Common/MonsterShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterShooting : MonoBehaviour
+{
+
+    private float timeBtwShots;
+    public float startTimeBtwnShots;
+    public GameObject Bullet;
+    
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwShots = startTimeBtwnShots;
+    }
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (timeBtwShots <=0)
+        {
+            Instantiate(Bullet, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwnShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+     
+    
+}
