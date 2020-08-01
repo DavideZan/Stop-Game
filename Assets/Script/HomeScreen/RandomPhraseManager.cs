329a6a0341774ffafccafa47ef01ed63afee7bf5
Annopaolo <arnaldo.cesco@gmail.com>
2020-08-01 20:07:18 +0200

    Homescreen fun
    with sentences!

---
+++ Assets/Script/HomeScreen/RandomPhraseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomPhraseManager : MonoBehaviour
+{
+    [TextArea]
+    public string[] phrases;
+    private Text box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<Text>();
+        var n = Random.Range(0, phrases.Length);
+                //7;
+        box.text = phrases[n];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
