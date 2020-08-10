216d9a3d5ed6c4e7b30ba05c83af4f415b18f8c8
Annopaolo <arnaldo.cesco@gmail.com>
2020-08-10 22:05:17 +0200

    Moving bridge
    and not much more

---
+++ Assets/Script/Abbattoggia2/ClosedBridgeCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosedBridgeCollision : MonoBehaviour
+{
+    public ShootingSceneController controller;
+
+    void Start()
+    {
+        controller = (ShootingSceneController) FindObjectOfType(typeof(ShootingSceneController));
+    }
+
+    // called when smth hits the closed bridge
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAA COLLISION");
+        Debug.Log("AAAAAAAAAAAAAa " + col.gameObject);
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+            controller.KillPlayer();
+        }    
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAA COLLISION");
+        Debug.Log("AAAAAAAAAAAAAa " + other.gameObject);
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            controller.KillPlayer();
+        }
+    }
+}
