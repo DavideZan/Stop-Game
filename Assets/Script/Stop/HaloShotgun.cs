7fccffc3289275fa5515be09bdceffd7d0a5fa9c
Annopaolo <arnaldo.cesco@gmail.com>
2020-08-20 15:51:17 +0200

    Stop scene
    We have Halo, too!

---
+++ Assets/Script/Stop/HaloShotgun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaloShotgun : ShootableObject
+{
+    public StopController controller;
+
+    void Start() {
+        controller = (StopController) FindObjectOfType(typeof(StopController));    
+    }
+
+    override public void OnShoot()
+    {
+        controller.GainHaloShotgun();
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Script/Stop/StopController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopController : ShootingSceneController
+{
+    public GameObject haloShotgunPrefab;
+
+    public void GainHaloShotgun()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject haloShotgun = Instantiate(haloShotgunPrefab, 
+                                                player.transform.position, 
+                                                Quaternion.Euler(0f, 180f, 0f),
+                                                player.transform);
+        haloShotgun.transform.localPosition = new Vector3(3f,0f,0f);
+        haloShotgun.transform.localScale = new Vector3(.4f,.4f,1f);
+        haloShotgun.GetComponent<SpriteRenderer>().sortingLayerName = "Entities";
+    }
+        
+    protected void RemoveHaloShotgun()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Destroy(player.transform.GetChild(0));
+    }
+}
