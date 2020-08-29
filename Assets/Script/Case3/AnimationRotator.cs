a7152dd8549a3750653ec2d45c4d2a0fda61dc6e
Annopaolo <arnaldo.cesco@gmail.com>
2020-08-29 15:51:02 +0200

    Folgarida

---
+++ Assets/Script/Case3/AnimationRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        if (players.Length > 0) {
+            var player = players[0];
+            if (player != null){
+                if (transform.position.x < player.transform.position.x){
+                    transform.rotation = Quaternion.Euler(0f,180f,0f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/Case3/FolgaridaEnemyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolgaridaEnemyGenerator : EnemyGenerator
+{ 
+    //TODO: enemyPrefab is to be chosen from a list of animations
+    override protected void GenerateEnemy()
+    {
+        float enemyY = Random.Range(minY, maxY);
+        float enemyX = Random.Range(minX, maxX);
+        GameObject enemy = Instantiate(enemyPrefab, new Vector2(enemyX , enemyY), Quaternion.identity, this.gameObject.transform);
+        enemy.GetComponent<SpriteRenderer>().sortingLayerName = "Entities";
+        enemy.transform.localScale = new Vector3(0.03f, 0.03f, 1f);
+    }
+}
