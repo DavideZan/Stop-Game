991cafe924bd00723aedf261000107b526016f3f
Davide <zanna_davide@hotmail.it>
2020-09-14 21:28:22 +0200

    Traghetto barrel

---
+++ Assets/RigiGeneralMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigiGeneralMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float thrust = 10.0f;
+
+    private void Start()
+    {
+        rb2D = gameObject.AddComponent<Rigidbody2D>();
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        }
+    }
+}
