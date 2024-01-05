c504177bd815a2ad5e212e69769b7a947dd45691
Arnaldo Cesco <arnaldo.cesco@gmail.com>
2024-01-05 11:58:26 +0100

    2024 Let's goooooo
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@gmail.com>

--- Assets/Script/Giardini/EnemyGeneratorGiardini.cs
+++ Assets/Script/Giardini/EnemyGeneratorGiardini.cs
@@ -7,13 +7,8 @@ public class EnemyGeneratorGiardini : MonoBehaviour
 {
     public float enemyY;
     public float enemyZ;
-    int[] spawnX = new int[3] { 0, 2, 9 };
-    bool[] posOccupied = new bool[3] { false, false, false };
-    [SerializeField] private float myDelay = 1f;
-    
-
-    //Random ran = new Random();
-    //public float objectOnScreen;
+    float[] spawn = new float[3] { 0, 2, 9 };
+    public float objectOnScreen;
     public GameObject enemyPrefab;
     
 
@@ -25,40 +20,24 @@ public class EnemyGeneratorGiardini : MonoBehaviour
     }
 
     // Update is called once per frame
-    /* void Update()
-     {
-         if (gameObject.transform.childCount < objectOnScreen)
-         {
-             GenerateEnemy();
+    void Update()
+    {
+        if (gameObject.transform.childCount < objectOnScreen)
+        {
+            GenerateEnemy();
+            
+        }
+    }
 
-         }
-     }
-
-     void GenerateEnemy()
-     {
-         System.Random ran = new System.Random();
-         float enemyX = spawn[ran.Next(spawn.Length)];
-        GameObject enemy = Instantiate(enemyPrefab, new Vector3(enemyX, enemyY, enemyZ), Quaternion.identity, this.gameObject.transform);
-        enemy.GetComponent<SpriteRenderer>().sortingLayerName = "Entities";
-
-     }
-     */
     void GenerateEnemy()
     {
-        for ( int i = 0; i < posOccupied.Length; i++)
-        {
-            if (!posOccupied[i])
-            {
-                GameObject enemy = Instantiate(enemyPrefab, new Vector3(spawnX[i], enemyY, enemyZ), Quaternion.identity, this.gameObject.transform);
-                enemy.GetComponent<SpriteRenderer>().sortingLayerName = "Entities";
-                posOccupied[i] = true;
-                Invoke("MyFunction", myDelay);
-
-            }
-        }
+        System.Random ran = new System.Random();
+        float enemyX = spawn[ran.Next(spawn.Length)];
+       GameObject enemy = Instantiate(enemyPrefab, new Vector3(enemyX, enemyY, enemyZ), Quaternion.identity, this.gameObject.transform);
+       enemy.GetComponent<SpriteRenderer>().sortingLayerName = "Entities";
+       
     }
 }
 
- 
 
 
